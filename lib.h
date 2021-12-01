4f2013e50f828ed99bee509fcf13c7780f462903
Matteo2244 <91881036+Matteo2244@users.noreply.github.com>
2021-12-01 20:42:59 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,9 +2,8 @@
 // Created by Matteo Mulvoni on 01/12/21.
 //
 
-#ifndef ESERCIZIO_LIBRERIE_H
-#define ESERCIZIO_LIBRERIE_H
+#ifndef ESERCIZIO_LIB_H
+#define ESERCIZIO_LIB_H
 
-#endif //ESERCIZIO_LIBRERIE_H
-
+#endif //ESERCIZIO_LIB_H
 bool conversione (char&);
