c13046e6500d4e416b1da95894238f33e444c05a
Matteo2244 <91881036+Matteo2244@users.noreply.github.com>
2021-12-01 20:24:40 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,5 +1,17 @@
-//
-// Created by Matteo Mulvoni on 01/12/21.
-//
-
 #include "librerie.h"
+bool conversione (char&a) {
+    bool t=true;
+    if(a>=65 and a<=90 or a>=97 and a<=122){
+        t=true;
+        if(a>=65 and a<=90){
+            a=a+32;
+            return a;
+        }else{
+            a=a-32;
+            return a;
+        }
+    }else{
+        t=false;
+        }
+    return t;
+    }
